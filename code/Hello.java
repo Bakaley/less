d466bf240510fc9f638774ffbaf139eecd4cb9bd
L <bakaley62@gmail.com>
2020-05-04 01:14:36 +0300

    test1

--- code/Hello.java
+++ code/Hello.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Hello {
+
+
     public static void main(String[] args) {
-        System.out.println("q23");
-        System.out.println("s12s");
+
+        double first;
+        double second;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число");
+
+        while(!(scanner.hasNextDouble())){
+            System.out.println("Пожалуйста, введите  число");
+            scanner.next();
+        }
+
+        first = scanner.nextDouble();
+
+        System.out.println("Введите второе число");
+
+        while(!(scanner.hasNextDouble())){
+            System.out.println("Пожалуйста, введите  число");
+            scanner.next();
+        }
+
+        second = scanner.nextDouble();
+
+        System.out.println("Сумма: " + (first+second));
+        System.out.println("Разность: " + (first-second));
+        System.out.println("Произведение: " + (first*second));
+        System.out.println("Частное: " + (first/second));
     }
 }
