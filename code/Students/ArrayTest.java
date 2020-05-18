55da7daa83474ed71bcd8f103666b15a6a2ea6ce
L <bakaley62@gmail.com>
2020-05-18 20:01:52 +0300

    test1

---
+++ code/Students/ArrayTest.java
@@ -0,0 +1,56 @@
+package Students;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+
+        MyArrayList myArrayList = new MyArrayList();
+
+        System.out.println("Закидываем 10 чисел в массив и печатаем:");
+        for (int i = 0; i < 10; i++){
+            myArrayList.add(ThreadLocalRandom.current().nextInt(-10, 10));
+        }
+        System.out.println(myArrayList);
+
+        System.out.println("Сортируем и печатаем: ");
+        myArrayList.sort();
+        System.out.println(myArrayList);
+
+        int numToAdd = 20;
+        System.out.println("Добавляем число " + numToAdd + " и печатаем его индекс: ");
+        myArrayList.add(numToAdd);
+        System.out.println(myArrayList + " [" + myArrayList.getIndex(numToAdd) + "]");
+
+        System.out.println("Добавляем массив {0, 1, 2} и печатаем: ");
+        myArrayList.add (new int[]{0, 1, 2});
+        System.out.println(myArrayList);
+
+        System.out.println("Добавляем MyArrayList {3, 4, 5} и печатаем: ");
+        myArrayList.add(new MyArrayList(new int[]{3, 4, 5}));
+        System.out.println(myArrayList);
+
+        int numToFind = -19;
+        System.out.println("Пытаемся найти индекс элемента " + numToFind + ", которого нет: " + myArrayList.getIndex(numToFind));
+
+        int numToCheck = ThreadLocalRandom.current().nextInt(-10, 10);
+        System.out.println("Есть ли в массиве элемент " + numToCheck + ": " + myArrayList.contains(numToCheck));
+
+        int index = 2;
+        System.out.println("Печатаем элемент по индексу: [" + index + "]");
+        System.out.println(myArrayList);
+        System.out.println(myArrayList.get(2));
+
+        MyArrayList arrayListToCompare1 = new MyArrayList(new int[]{0, 1, 2});
+        MyArrayList arrayListToCompare2 = new MyArrayList(new int[]{0, 1});
+        arrayListToCompare2.add(2);
+
+        System.out.println("Создаем листы {0, 1, 2} и {0, 1}, закидываем во второй массив 2 и сравниваем: " + arrayListToCompare1.equals(arrayListToCompare2));
+        System.out.println("Равны ли массивы {0, 1, 2} и первосозданный: " + arrayListToCompare1.equals(myArrayList));
+        System.out.println("Равны ли массивы {0, 1, 2} и {0, 1, 3}: " + new MyArrayList(new int[]{0, 1, 2}).equals(new MyArrayList(new int[]{0, 1, 3})));
+
+        System.out.println("Очищаем и печатаем: ");
+        myArrayList.clear();
+        System.out.println(myArrayList);
+    }
+}
---
+++ code/Students/MyArrayList.java
@@ -0,0 +1,172 @@
+package Students;
+
+public class MyArrayList {
+
+    private int[] array = new int[0];
+
+    MyArrayList () {
+
+    }
+
+    MyArrayList (int[] arrayToCreate){
+        if(arrayToCreate.length!=0) this.array = arrayToCreate;
+    }
+
+    public void add (int numberToAdd) {
+        if(array.length==0){
+            array = new int[1];
+            array[0] = numberToAdd;
+        }
+        else {
+            int[] newArray = new int[array.length+1];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            newArray[newArray.length-1] = numberToAdd;
+            array = newArray;
+        }
+    }
+
+    public int getIndex (int numToFind) {
+        if(array.length==0) {
+            return -1;
+        }
+        else {
+            for (int i = 0; i < array.length; i++) {
+                if(array[i] == numToFind)  return i;
+            }
+        }
+        return -1;
+    }
+
+    public int get (int index){
+        return array[index];
+    }
+
+    public boolean contains (int numToFind) {
+        if(array.length==0) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < array.length; i++) {
+                if(array[i] == numToFind)  return true;
+            }
+        }
+        return false;
+    }
+
+    public void add(int[] arrayToAdd){
+
+        if(arrayToAdd.length==0) return;
+
+        else if(array.length==0){
+            array = arrayToAdd;
+        }
+
+        else {
+            int[] newArray = new int[array.length + arrayToAdd.length];
+
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+
+            for (int i = 0; i < arrayToAdd.length; i++) {
+                newArray[i+array.length] = arrayToAdd[i];
+            }
+
+            array = newArray;
+        }
+    }
+
+    public void add(MyArrayList arrayToAdd){
+
+        if(arrayToAdd.array.length==0) return;
+
+        else if(array.length==0){
+            array = arrayToAdd.array;
+        }
+
+        else {
+            int[] newArray = new int[array.length + arrayToAdd.array.length];
+
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+
+            for (int i = 0; i < arrayToAdd.array.length; i++) {
+                newArray[i+array.length] = arrayToAdd.array[i];
+            }
+
+            array = newArray;
+        }
+    }
+
+    public int getSize (){
+        return array.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder("{");
+        if(array.length==0)  return "{}";
+        else for (int i = 0; i <array.length; i++) {
+            if(i == array.length-1) string.append(array[i] + "}");
+            else string.append(array[i] + ", ");
+        }
+        return string.toString();
+    }
+
+    public boolean equals (MyArrayList listToCompare){
+
+        if(!(this.getSize() == listToCompare.getSize())) return false;
+
+        else if(this.getSize() == 0 && listToCompare.getSize() == 0) return true;
+
+        else {
+            for (int i = 0; i < this.array.length; i++) {
+                if(this.array[i] == listToCompare.array[i]);
+                else return false;
+            }
+            return true;
+        }
+    }
+
+    public void clear(){
+        array = new int[0];
+    }
+
+    public void sort (){
+
+        boolean flag = true;
+        int leftBound = 0;
+        int rightBound = array.length-1;
+
+        while ((leftBound < rightBound) && flag){
+
+            flag = false;
+
+            for (int i = leftBound; i < rightBound; i++) {
+                if(array[i] > array[i+1]) {
+                    int temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    flag = true;
+                }
+            }
+
+            rightBound--;
+
+            for (int i = rightBound; i > leftBound; i--) {
+                if(array[i-1] > array[i]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    flag = true;
+                }
+            }
+
+            leftBound++;
+        }
+    }
+
+}
